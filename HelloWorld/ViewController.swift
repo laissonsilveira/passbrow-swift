54774c8e2c2fb3fb0f0a00196aa471c9613442b1
Laisson R. Silveira <laissonsilveira@MacBook-Air-de-Laisson.local>
2015-01-25 19:46:12 -0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Laisson R. Silveira on 1/25/15.
+//  Copyright (c) 2015 Laisson R. Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
